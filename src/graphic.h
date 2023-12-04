fd124a0f1de8870c842329123df5728249cd7bc2
Ivan Gousev <vanyagousev@gmail.com>
2023-12-04 18:53:42 +0300

    stable window

--- src/graphic.h
+++ src/graphic.h
@@ -9,37 +9,62 @@
 #include <FL/Fl_Widget.H>
 #include <FL/Enumerations.H>
 #include "determinant.h"
+#include <FL/Fl_Text_Editor.H>
+#include <FL/Fl_Menu_Item.H>
+#include <FL/Fl_Menu_Bar.H>
+#include <FL/Fl_Scroll.H>
+#include <FL/Fl_File_Chooser.H>
 
-class Window_Maker
+class WINDOW
 {
-private:
-    Fl_Window *window;
 public:
-    void Window(int width, int height);
-    Fl_Button *Button(int posX, int posY, int width, int height, const char *name);
+    Fl_Window *window;
+    void MakeWindow(int width, int height);
+    Fl_Button *MakeButton(int posX, int posY, int width, int height, const char *name);
+    Fl_Menu_Bar *MakeBar(int posX, int posY, int width, int height, Fl_Menu_Item menuitems[]);
 };
 
 
-namespace Main_Window{
-    static Window_Maker MAKE;
+
+
+class FileBrowser : public WINDOW{
+public:
+    void init();
+    FileBrowser();
+    Fl_File_Chooser *browser;
+};
+
+
+class Main_Window : public WINDOW{
+public:
     void init();
 
+
+    FileBrowser brow;
+    std::vector<Fl_Button *> buttons = std::vector<Fl_Button *>((size_t)10, nullptr);  //более 10 кнопок нам не потребуется
+    enum class Buttons{
+        mode_change=0,
+        open_file_exp=1
+    };
     enum class Event{
-        file_exp_called,
-        matrix_chaenged,
-        menu_called,
-        error_catched
+        file_exp_called=1,
+        matrix_chaenged=2,
+        menu_called=3,
+        error_catched=4
     };
 
-
-    void event_handler(Event type);
 
     void matrix_drow(int posX, int posY, int width, int height, const char *title, Matrix matrix);
     void matrix_redrow();
     
     void refreash();
-}
-namespace Main_Window{
+};
+
+
+
+/*
+namespace Mode_Selector{
     static Window_Maker MAKE;
-}
+    void init();
+}*/
 #endif //__GRAPHIC_H__
